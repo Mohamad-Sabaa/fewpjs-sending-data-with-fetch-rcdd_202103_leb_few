93eebee00892b152950bcc0832a44c1971703379
Mohamad  Al Sabaa <mohamadsabaa3@gmail.com>
2021-05-15 11:45:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ function submitData(name, email) {
   return fetch('http://localhost:3000/users', {
     method: "POST",
     headers: {
-      "Content-type"= "application/json",
-      "Accept" = "application/json"
+      "Content-type": "application/json",
+      "Accept": "application/json"
     },
     body: JSON.stringify({
       name,
       email
     })
   })
+  .then(resp => resp.json())
+  .then(obj => console.log(obj))
 }
